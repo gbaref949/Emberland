69fedf65859a691975fd60e5e74625ba2c584e58
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-02-21 14:10:15 -0700

    added changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Game1 from "./components/Home";
-import Testing from './components/Testing';
+// import Game1 from "./components/Home";
+import Level1 from './components/Level1';
 
 function App() {
   return (
     <div className="App">
       {/* <Game1/> */}
-      <Testing></Testing>
+      <Level1/>
     </div>
   );
 }
--- client/src/components/Level1.jsx
+++ client/src/components/Level1.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from 'react';
+import './styles.css'; // Importing the CSS file
 
-const Level1 = () => {
-  return (
-    <div>Level1</div>
-  )
+class Block {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 4; // Speed of the block
+  }
+
+  update() {
+    // Check for key presses and move the block accordingly
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+      this.x -= this.speed;
+    }
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+      this.x += this.speed;
+    }
+    if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+      this.y -= this.speed;
+    }
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+      this.y += this.speed;
+    }
+
+    // Constrain the block within the canvas boundaries
+    this.x = constrain(this.x, 0, window.innerWidth - blockSize);
+    this.y = constrain(this.y, 0, window.innerHeight - blockSize);
+  }
+
+  show() {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          left: this.x,
+          top: this.y,
+          width: blockSize,
+          height: blockSize,
+          backgroundColor: 'red',
+        }}
+      ></div>
+    );
+  }
 }
 
-export default Level1
+class Level1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.block = null;
+    this.blockSize = 50;
+  }
+
+  componentDidMount() {
+    this.block = new Block(window.innerWidth / 2, window.innerHeight / 2);
+    window.addEventListener('keydown', this.handleKeyPress);
+    this.interval = setInterval(this.updateGame, 16); // 60 frames per second
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+    clearInterval(this.interval);
+  }
+
+  handleKeyPress = (event) => {
+    // Handle key presses
+    // You can implement the logic for moving the block here
+  };
+
+  updateGame = () => {
+    this.block.update();
+    this.forceUpdate(); // Force re-render to update block position
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="game-container">{this.block.show()}</div>
+      </div>
+    );
+  }
+}
+
+export default Level1;
