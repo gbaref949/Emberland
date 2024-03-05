68c6487139e00532fd29975b40ae559267fdddc0
NoahSantos <noahbsantos@gmail.com>
2024-03-04 22:07:48 -0700

    Controls
    Added a back button in order to get back to the dashboard from the leaderboard

--- client/src/pages/Controls.jsx
+++ client/src/pages/Controls.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
 
 const Controls = () => {
   return (
     <div className="controls-container">
+      <Link to={'/dashboard'} className='backBtn'><IoIosArrowBack />Dashboard</Link>
       <h2>Controls</h2>
       <div className="control-info">
         <p><strong>W:</strong> Walk up</p>
