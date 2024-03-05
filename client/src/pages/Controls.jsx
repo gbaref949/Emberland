f970524fe7e6d046e73c79db5f23656a93e19467
NoahSantos <noahbsantos@gmail.com>
2024-03-04 21:55:29 -0700

    Linking
    Did some pathing to add the victory screen and did some restyling for the controls page

--- client/src/pages/Controls.jsx
+++ client/src/pages/Controls.jsx
@@ -5,7 +5,7 @@ const Controls = () => {
     <div className="controls-container">
       <h2>Controls</h2>
       <div className="control-info">
-        <p><strong>w:</strong> Walk up</p>
+        <p><strong>W:</strong> Walk up</p>
         <p><strong>A:</strong> Walk left</p>
         <p><strong>D:</strong> Walk right</p>
         <p><strong>S:</strong> Walk down</p>
