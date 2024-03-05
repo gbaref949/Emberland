0ac962de84d4109e526189e8a1a66bc4997a5e61
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-03-04 21:50:22 -0700

    made changes to files

--- client/src/pages/Controls.jsx
+++ client/src/pages/Controls.jsx
@@ -5,11 +5,11 @@ const Controls = () => {
     <div className="controls-container">
       <h2>Controls</h2>
       <div className="control-info">
-        <p><strong>w:</strong> Walk up</p>
+        <p><strong>W:</strong> Walk up</p>
         <p><strong>A:</strong> Walk left</p>
         <p><strong>D:</strong> Walk right</p>
         <p><strong>S:</strong> Walk down</p>
-        <p><strong>esc:</strong> Open menu</p>
+        <p><strong>ESC:</strong> Open menu</p>
         <p><strong>Left click:</strong> Slash</p>
         <p><strong>Shift:</strong> Dash</p>
       </div>
